1f10337bec40bd0ba5ad2f837cbfe188e80246d9
ruby73 <ouueyeb1@gmail.com>
2020-09-28 16:23:35 +0900

    16:23

---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+const TodoInput = () => {
+    return <>
+    <h1>할일 등록</h1>
+    <form action="">
+        <div>
+            <input type="text" name="todo"/><br/>
+            <input type="submit" value="SUBMIT"/>
+        </div>
+    </form>
+    </>
+}
+export default TodoInput
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+const TodoList = () => {
+    return <></>
+}
+export default TodoList 
