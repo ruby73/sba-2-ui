ce5cbe56499e6e868dabbad585c4fcc73be7959c
ruby73 <ouueyeb1@gmail.com>
2020-09-29 13:03:00 +0900

    13:02

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { Provider } from 'react-redux'
 import TodoInput from './components/TodoInput'
-import TodoList from './components/TodoList';
+import TodoList from './components/TodoList'
+import store from './store'
+
 
 const App = () => {
-  return <div style={{width: "200px",margin: "0 auto"}}>
-    <TodoInput/>
-    <TodoList/>
-  </div>
+  return <>
+    <Provider store={store}>
+      <div style={{ width: "200px", margin: "0 auto" }}>
+        <TodoInput /> <br />
+        <TodoList />
+      </div></Provider>
+  </>
 }
 
 export default App;
